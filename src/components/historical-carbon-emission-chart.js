ac2ba0702f5d3ebea74a697c38521ea8f4e27ac9
Rakesh <rakesh22717@gmail.com>
2020-07-02 05:16:23 -0700

    flight component started

--- src/components/historical-carbon-emission-chart.js
+++ src/components/historical-carbon-emission-chart.js
@@ -4,16 +4,18 @@ import CanvasJSReact from '../assets/canvasjs.react';
 
 const HistoricalCarbonEmissionChart = () => {
 
-    const foodUrl = baseUrl + "/carbon-historical-data"
+    const historicalCarbonUrl = baseUrl + "/carbon-historical-data"
+    const historicalTemperatureUrl = baseUrl + "/temperature-historical-data"
     let temp = [], tempArr = []
     var CanvasJSChart = CanvasJSReact.CanvasJSChart;
     const [chartData, setChartData] = useState([])
+    const [chartTemperatureData, setChartTemperatureData] = useState([])
 
     /* fetching data */
     function getLocalCFP() {
         // GET request using fetch with set headers
         const headers = { 'Content-Type': 'application/json' }
-        fetch(foodUrl, { headers })
+        fetch(historicalCarbonUrl, { headers })
             .then(response =>
                 response.json())
             .then(data => {
@@ -31,12 +33,38 @@ const HistoricalCarbonEmissionChart = () => {
             })
     }
 
+    /* fetching data */
+    function getTemperatureCFP() {
+        // GET request using fetch with set headers
+        let tempArr1 = [], temp1
+        const headers = { 'Content-Type': 'application/json' }
+        fetch(historicalTemperatureUrl, { headers })
+            .then(response =>
+                response.json())
+            .then(data => {
+
+                data[1].result.forEach(element => {
+
+                    temp1 = ({
+                        x: new Date(element.year),
+                        y: element.temperature
+                    })
+                    tempArr1.push(temp1)
+                });
+               // setChartTemperatureData(tempArr)
+                //console.log("getTemperatureCFP -> tempArr", tempArr)
+                console.log("getTemperatureCFP -> tempArr1", tempArr1)
+                return [tempArr1]
+            })
+        
+    }
+
     const options = {
         animationEnabled: true,
         animationDuration: 2500,
         backgroundColor: "transparent",
         theme: "dark1",
-        lineDashType: "dash" ,
+        lineDashType: "dash",
         legend: {
             horizontalAlign: "right", // "center" , "right"
             verticalAlign: "top",  // "top" , "bottom"
@@ -73,7 +101,18 @@ const HistoricalCarbonEmissionChart = () => {
                     labelAlign: "center",
                     labelBackgroundColor: "white"
                 }
-            ]
+            ],
+            includeZero: true
+        },
+        axisY2: {
+            title: "temperature increase",
+            titleFontColor: "#51CDA0",
+            // interval: 20,
+            suffix: " c",
+            lineColor: "#51CDA0",
+            labelFontColor: "#51CDA0",
+            tickColor: "#51CDA0",
+            includeZero: true
         },
         data: [
             {
@@ -81,12 +120,21 @@ const HistoricalCarbonEmissionChart = () => {
                 showInLegend: true,
                 legendText: "Carbon Emission",
                 dataPoints: chartData
+            },
+            {
+                type: "splineArea",
+                axisYType: "secondary",
+                showInLegend: true,
+                legendText: "Average temperature increase",
+                dataPoints: getTemperatureCFP() 
+
             }
         ]
     }
 
     useEffect(() => {
         getLocalCFP()
+        //getTemperatureCFP()
     }, [])
 
 
--- src/pages/flight.js
+++ src/pages/flight.js
@@ -1,76 +1,95 @@
 import React, { useState, useEffect } from 'react';
-import usePlacesAutocomplete, {
-    getGeocode,
-    getLatLng,
-} from "use-places-autocomplete";
-import useOnclickOutside from "react-cool-onclickoutside";
+import { Container, Button } from '@material-ui/core';
+import TakeOffInput from '../components/flight/take-off-input.js'
+import DestinationInput from '../components/flight/destination-input.js'
+import ClassDropdown from '../components/flight/class-dropdown.js'
+import RadioTripChoice from '../components/flight/radio-trip-choice.js'
+import Result from '../components/flight/result.js'
 
 const Flight = () => {
-    const {
-        ready,
-        value,
-        suggestions: { status, data },
-        setValue,
-        clearSuggestions,
-    } = usePlacesAutocomplete({
-        requestOptions: {
-            /* Define search scope here */
-        },
-        debounce: 300,
-    });
-    const ref = useOnclickOutside(() => {
-        // When user clicks outside of the component, we can dismiss
-        // the searched suggestions by calling this method
-        clearSuggestions();
-    });
 
-    const handleInput = (e) => {
-        // Update the keyword of the input element
-        setValue(e.target.value);
-    };
+    // variable declaration
+    const [takeOff, setTakeOff] = useState()
+    const [takeOffAddress, setTakeOffAddress] = useState("")
+    const [destination, setDestination] = useState()
+    const [destinationAddress, setDestinationAddress] = useState("")
+    const [classType, setClassType] = useState()
+    const [trip, setTrip] = useState() //by default 0 = "one way trip"
+    const [resultLoader, setResultLoader] = useState(false) //by default 0 = "one way trip"
+    const [flightClassOption, setFlightClassOption] = useState([]) 
 
-    const handleSelect = ({ description }) => () => {
-        // When user selects a place, we can replace the keyword without request data from API
-        // by setting the second parameter as "false"
-        setValue(description, false);
-        clearSuggestions();
 
-        // Get latitude and longitude via utility functions
-        getGeocode({ address: description })
-            .then((results) => getLatLng(results[0]))
-            .then(({ lat, lng }) => {
-                console.log("📍 Coordinates: ", { lat, lng });
-            })
-            .catch((error) => {
-                console.log("😱 Error: ", error);
-            });
-    };
+    // calculate carbon footprint
+    const calculateCarbonFootPrint = () => {
+        if (takeOff !== undefined && destination !== undefined && classType !== undefined && trip !== undefined) {
+            setResultLoader(true)
+        }
+    }
 
-    const renderSuggestions = () =>
-        data.map((suggestion) => {
-            const {
-                id,
-                structured_formatting: { main_text, secondary_text },
-            } = suggestion;
+    useEffect(() => {
+        calculateCarbonFootPrint()
+        return () => {
+            // cleanup
+        };
+    }, [takeOff, destination, classType, trip]);
 
-            return (
-                <li key={id} onClick={handleSelect(suggestion)}>
-                    <strong>{main_text}</strong> <small>{secondary_text}</small>
-                </li>
-            );
-        });
+    // callback from take off to set lat lng
+    const handleTakeOffCallback = (data, address) => {
+        setTakeOff(data)
+        setTakeOffAddress(address)
+    }
+
+    // callback from take off to set lat lng
+    const handleDestinationCallback = (data, address) => {
+        setDestination(data)
+        setDestinationAddress(address)
+    }
+
+    // callback from drop down (flight class type)
+    const handleDropDownCallback = (data, options) => {
+        setClassType(data)
+        setFlightClassOption(options)
+    }
+
+    // callback from trip choice (one way ur return)
+    const handleTripChoiceCallback = (data) => {
+        setTrip(data)
+    }
+
 
     return (
-        <div ref={ref}>
-            <input
-                value={value}
-                onChange={handleInput}
-                disabled={!ready}
-                placeholder="Where are you going?"
-            />
-            {/* We can use the "status" to decide whether we should display the dropdown or not */}
-            {status === "OK" && <ul>{renderSuggestions()}</ul>}
-        </div>
+        <Container className="flight-main-container">
+            {/* heading */}
+            <h1>How do your Air Flight Travel impact the Environment?</h1>
+
+            {/* logo+input container  */}
+            <div className="logo+input-container">
+                {/* Logo Container */}
+                <div className="logo-container">
+                    {/* flight logo */}
+                    {/* <img /> logo */}
+                    <h3>Air Flight</h3>
+                </div>
+
+                {/* input container */}
+                <div className="input-container">
+                    {/* input 1 */}
+                    <TakeOffInput parentCallback={handleTakeOffCallback} />
+                    {/* input 2 */}
+                    <DestinationInput parentCallback={handleDestinationCallback} />
+                </div>
+
+                {/* flight class choice  */}
+                <ClassDropdown parentCallback={handleDropDownCallback} />
+                {/* trip choice */}
+                <RadioTripChoice parentCallback={handleTripChoiceCallback} />
+
+                {/* show result */}
+                {resultLoader ? <Result takeOff={takeOff} destination={destination} takeOffAddress={takeOffAddress} destinationAddress={destinationAddress} classType={classType} trip={trip} flightClassOption={flightClassOption} /> : null }
+
+            </div>
+
+        </Container>
     );
 }
 
