68695e0b75199d2b63ab5660988ef17b69e65f9a
Dalbir Singh <dalbirsrana@gmail.com>
2020-07-09 02:45:44 -0700

    Home page design

--- src/components/AppHeader/AppLogo.js
+++ src/components/AppHeader/AppLogo.js
@@ -5,7 +5,9 @@ import Logo from '../../images/Logo.svg';
 const AppLogo = () => (
     <div className="site-logo">
         <Link to="/">
-            <img src={Logo} alt="carbonPrint" height="70px" width="120px" /></Link>
+            {/* <img src={Logo} alt="carbonPrint" height="70px" width="120px" /> */}
+            <h2>Carbon Print</h2>
+        </Link>
     </div>
 );
 
