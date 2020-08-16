d17dfc7f9aab5f9886c3e92e3577c2b2d524464e
Rakesh <rakesh22717@gmail.com>
2020-08-16 13:18:30 -0700

    minor db fix

--- src/database-secrets/secrets.js
+++ src/database-secrets/secrets.js
@@ -1,6 +1,6 @@
 // const db_password = "smack$1"
 const baseUrl = "https://api.carbonprint.wmdd.ca"
-//const baseUrl = "http://localhost"
+// const baseUrl = "http://localhost"
 
 export default (
     baseUrl
